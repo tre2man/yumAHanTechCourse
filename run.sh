6fcef927a6762c9716d36df428035626975d98a0
Namwoo Kim <kimtree3940@gmail.com>
2022-01-16 19:23:08 +0900

    Feat : 실행 스크립트 추가

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker stop $(docker ps -aq)                      # 도커 컨테이너 전체 멈춤
+docker rm $(docker ps -aq)                      # 도커 컨테이너 전체 삭제
+docker rmi $(docker images -q)                    # 도커 이미지파일 전체 삭제
+docker build -f Dockerfile --tag yumTecho . # 새로운 도커 이미지 생성
+docker run -d -p5000:5000 yumTecho              # 5000번 포트로 접속할 수 있는 도커 이미지 실행하기
