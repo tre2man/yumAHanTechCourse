1439d820ac393da512d8d9dc31c8ebab3e42e814
Namwoo Kim <kimtree3940@gmail.com>
2022-01-16 19:24:43 +0900

    Feat : 도커 대문자 -> 소문자

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker stop $(docker ps -aq)                      # 도커 컨테이너 전체 멈춤
 docker rm $(docker ps -aq)                      # 도커 컨테이너 전체 삭제
 docker rmi $(docker images -q)                    # 도커 이미지파일 전체 삭제
-docker build -f Dockerfile --tag yumTecho . # 새로운 도커 이미지 생성
-docker run -d -p5000:5000 yumTecho              # 5000번 포트로 접속할 수 있는 도커 이미지 실행하기
+docker build -f Dockerfile --tag yum-techo . # 새로운 도커 이미지 생성
+docker run -d -p5000:5000 yum-techo              # 5000번 포트로 접속할 수 있는 도커 이미지 실행하기
